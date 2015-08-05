85298f01d4cbae80ff3a6b74acafc79794f1ba31
mahehss <mahexh@gmail.com>
2015-08-05 18:19:06 +0530

    added new code

---
+++ js/main.js
@@ -0,0 +1,140 @@
+jQuery(function($) {'use strict',
+
+	//#main-slider
+	$(function(){
+		$('#main-slider.carousel').carousel({
+			interval: 8000
+		});
+	});
+
+
+	// accordian
+	$('.accordion-toggle').on('click', function(){
+		$(this).closest('.panel-group').children().each(function(){
+		$(this).find('>.panel-heading').removeClass('active');
+		 });
+
+	 	$(this).closest('.panel-heading').toggleClass('active');
+	});
+
+	//Initiat WOW JS
+	new WOW().init();
+
+	// portfolio filter
+	$(window).load(function(){'use strict';
+		var $portfolio_selectors = $('.portfolio-filter >li>a');
+		var $portfolio = $('.portfolio-items');
+		$portfolio.isotope({
+			itemSelector : '.portfolio-item',
+			layoutMode : 'fitRows'
+		});
+		
+		$portfolio_selectors.on('click', function(){
+			$portfolio_selectors.removeClass('active');
+			$(this).addClass('active');
+			var selector = $(this).attr('data-filter');
+			$portfolio.isotope({ filter: selector });
+			return false;
+		});
+	});
+
+	// Contact form
+	var form = $('#main-contact-form');
+	form.submit(function(event){
+		event.preventDefault();
+		var form_status = $('<div class="form_status"></div>');
+		$.ajax({
+			url: $(this).attr('action'),
+
+			beforeSend: function(){
+				form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn() );
+			}
+		}).done(function(data){
+			form_status.html('<p class="text-success">' + data.message + '</p>').delay(3000).fadeOut();
+		});
+	});
+
+	
+	//goto top
+	$('.gototop').click(function(event) {
+		event.preventDefault();
+		$('html, body').animate({
+			scrollTop: $("body").offset().top
+		}, 500);
+	});	
+
+	//Pretty Photo
+	$("a[rel^='prettyPhoto']").prettyPhoto({
+		social_tools: false
+	});	
+});
+
+(function($) {
+  var ourTeam = {
+    cwindow : '85%',
+    wwindow : $(window).width(),
+    init : function(){
+      ourTeam.wload();
+      ourTeam.resize();
+      ourTeam.topen();
+    },
+    wload : function(){
+      if(ourTeam.wwindow > 1024){
+        ourTeam.cwindow = '972px';
+      }
+    },
+    resize : function(){
+      $(window).resize(function(){
+        ourTeam.wwindow = $(this).width();
+        if(ourTeam.wwindow > 1024){
+          ourTeam.cwindow = '972px';
+        } else {
+          ourTeam.cwindow = '85%';
+        }
+      });
+    },
+    topen : function(){
+      $('.photo-container > .photo').click(function(){
+        var inline = $(this).parent('.photo-container').find('.text');
+        if(!$(this).parent('.photo-container').hasClass('no-bio')){
+          $.colorbox({
+            inline:true,
+            maxWidth:ourTeam.cwindow,
+            maxHeight: '95%',
+            fixed:true,
+            href: inline
+          });
+        }
+      });
+    }
+  };
+  ourTeam.init();
+
+
+var mq = window.matchMedia( "(min-width: 992px)" );
+if (mq.matches) {
+    // grayscale to color hover effect
+  $('.photo-container > .photo').hover(
+    function() {
+      $(this).children('img').first().removeClass('hidden');
+    },
+    function() {
+      $(this).children('img').first().addClass('hidden');
+    }
+  );
+}
+else {
+  // grayscale to color hover effect
+  $('.photo-container.no-bio > .photo').click(
+    function() {
+      $(this).children('img').first().toggleClass('hidden');
+      ourTeam.topen;
+    }/*,
+    function() {
+      $(this).children('img').first().addClass('hidden');
+
+    }*/
+  );
+}
+  
+})(jQuery);
